09035a4a5023bd52ece7fefd051ab29fc5a25717
Deyrai <deyraiconsulting@gmail.com>
2026-04-14 15:43:50 +0530

    Changes contact page

--- next.config.ts
+++ next.config.ts
@@ -3,22 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   trailingSlash: false,
-
-  async redirects() {
-    return [
-      {
-        source: "/contact",
-        destination: "/contact",
-        permanent: true,
-      },
-      {
-        source: "/:path*",
-        has: [{ type: "host", value: "www.nevatrix.com" }],
-        destination: "https://nevatrix.com/:path*",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
