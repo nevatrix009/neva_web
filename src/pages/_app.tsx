0f1d185c788de087b500d15eed79c8c4e640afa4
Deyrai <deyraiconsulting@gmail.com>
2026-05-08 18:46:02 +0530

    added robots txt

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -24,6 +24,15 @@ export default function App({ Component, pageProps }: AppProps) {
         `}
             </Script>
 
+            <Script id="microsoft-clarity" strategy="afterInteractive">
+                {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "wnwrmhx0t9");
+          `}
+            </Script>
 
 
             {/* Website Layout */}
